ba0bed2c5da44e6e0a4514d7b337a8e654860489
asif809999 <Professional100001@yahoo.com>
2023-10-28 17:27:37 +0600

    a riverpod Provider added inside user_places.dart file 13.6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:favorite_places_2/screens/places.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -27,7 +28,9 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
