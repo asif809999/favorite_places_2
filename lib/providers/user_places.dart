04c4d2a65ab8ab77fcbb4a8aa616e40640cb06f7
asif809999 <asif809999@gmail.com>
2024-01-24 13:46:42 +0600

    sql database used to store data 13.29

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 import 'package:favorite_places_2/models/place.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
@@ -16,6 +19,26 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       title: title,
       image: copiedImage,
       location: location,
+    );
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+      },
+      version: 1,
+    );
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'lat': newPlace.location.latitude,
+        'lng': newPlace.location.longtitude,
+        'address': newPlace.location.address,
+      },
     );
     state = [newPlace, ...state];
   }
